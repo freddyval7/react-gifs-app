c1a1d7dbb066d7c23df83094c953602a027e5c48
Freddy Tomada <freddytomada7@gmail.com>
2026-03-24 08:58:52 -0400

    fix errors type

--- src/gifs/interfaces/giphy.response.ts
+++ src/gifs/interfaces/giphy.response.ts
@@ -95,14 +95,9 @@ export interface FixedHeight {
   hash?: string;
 }
 
-export enum Rating {
-  G = "g",
-  R = "r",
-}
+export type Rating = "G" | "PG";
 
-export enum Type {
-  GIF = "gif",
-}
+export type Type = "gif" | "sticker";
 
 export interface User {
   avatar_url: string;
